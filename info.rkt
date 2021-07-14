c13dc1d567f2967f69600afd7110a5ed3b9b464d
yjqww6 <343519265@qq.com>
2021-07-14 15:35:38 +0800

    fix missing build-deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "rkt-tree-widget")
 (define deps '("base" "gui-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "gui-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/rkt-tree-widget.scrbl" ())))
 (define pkg-desc "Yet another tree widget for racket/gui")
 (define version "0.0")
